e7b455901574c7915bb8553e690660ae1084fb85
Carolyn Kao <chkao831@stanford.edu>
2021-05-04 01:06:53 -0700

    multiple sampling added

--- autotune.py
+++ autotune.py
@@ -1,15 +1,16 @@
-# Usage: $ python autotune.py <yaml_file>
 # Warning: The execution of this file will automatically remove *.log, 
 #          ctest-*.json and all auto-generated _[X].yaml files from this directory.
 #          Would potentially overwrite the original yaml file if the execution fails.
 
 # From Albany/tools - yaml read/write
 
+# Need: pip install --user pandas
 # Need: pip install --user ruamel.yaml
 # Need: pip install --user scikit-learn
-# Need: pip install --user pandas
 
 # Import libraries
+import argparse
+import getopt
 import glob
 import json
 import numpy as np
@@ -17,6 +18,7 @@ import os
 import pandas as pd
 import subprocess
 import sys
+from collections import Counter
 from sklearn.model_selection import ParameterGrid
 from ruamel.yaml import YAML
 
@@ -86,8 +88,8 @@ def grid_search(inFile):
     paramList = muDict['Factories']['mySmoother1']['ParameterList']
     
     # Define Grid: np.arange(start[inclusive], stop[exclusive], step)
-    param_grid = {'relaxation: damping factor': list(np.arange(0.4, 1.8, 0.1).round(decimals=2)),
-                  'relaxation: sweeps': list(np.arange(1, 5, 1))
+    param_grid = {'relaxation: damping factor': list(np.arange(0.8, 1.0, 0.1).round(decimals=1)),
+                  'relaxation: sweeps': list(np.arange(1, 2, 1))
                  # add more params here to tune if applicable
                  }
     grid = ParameterGrid(param_grid)
@@ -105,7 +107,7 @@ def grid_search(inFile):
         # Run yaml input file
         run_sim(ite, inFile)
         ite = ite + 1
-    print('Total #Iterations:', ite)
+    print('Total #iterations (cases):', ite)
     # Convert logs to json
     run_bash('python ctest2json.py')
     return iter_param_dict
@@ -119,7 +121,7 @@ def json_timers(filenames, case):
     Returns:
         iter_time_dict(dictionary): {#iter:Albany_Total_Time}
     '''
-    print("CASE: ", case)
+    #print("CASE: ", case)
     iter_time_dict = dict()
     for each_file in filenames:
         with open(each_file) as f:
@@ -177,28 +179,52 @@ if __name__ == "__main__":
     '''
     Run example
     '''
-    if len(sys.argv) != 2:
-        print('Usage: python autotune.py <yaml_file>')
-        sys.exit(2)
-    yaml_filename = sys.argv[1]
-    print("YAML FILENAME: ", yaml_filename)
+
+    yaml_filename = str()
+    num_simu = 1
+  
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str,
+                    help="YAML input filename")
+    parser.add_argument("-s", "--simulations", type=int,
+                    help="Number of sampled simulations [Default=1]")
+    args = parser.parse_args()
+    yaml_filename = args.filename
+    if args.simulations:
+        num_simu = args.simulations
+    print("YAML INPUT FILENAME: ", yaml_filename) 
+    print("NUM OF SIMULATION(s): ", num_simu)
+
     casename = get_casename(yaml_filename)
-    # remove previously-generated files: Tune_*.yaml, *.log, ctest-*.json from this directory
-    previous_yaml = yaml_filename.split('.')[0] + str("_*.yaml")
-    prev_yaml = glob.glob(previous_yaml, recursive=False) #input_albany_Velocity_MueLu_Wedge_Tune_*.yaml 
-    prev_log = glob.glob('*.log', recursive=False)
-    prev_ctest = glob.glob('ctest-*', recursive=False)
-    for filePath in (prev_log + prev_yaml + prev_ctest): 
-        try:
-            os.remove(filePath)
-        except OSError:
-            print("Error while deleting file")
-    
-    # perform grid search
-    iter_param_dict = grid_search(yaml_filename)
-    # post process: get timers from generated json files
-    out_filename = glob.glob('ctest-*.json')
-    iter_time_dict = json_timers(out_filename,casename)
+
+    iter_time_dict = dict()
+    for i in range(num_simu):
+        # remove previously-generated files: Tune_*.yaml, *.log, ctest-*.json from this directory
+        previous_yaml = yaml_filename.split('.')[0] + str("_*.yaml")
+        prev_yaml = glob.glob(previous_yaml, recursive=False) #input_albany_Velocity_MueLu_Wedge_Tune_*.yaml 
+        prev_log = glob.glob('*.log', recursive=False)
+        prev_ctest = glob.glob('ctest-*', recursive=False)
+        for filePath in (prev_log + prev_yaml + prev_ctest): 
+            try:
+                os.remove(filePath)
+            except OSError:
+                print("Error while deleting file")
+        # perform grid search
+        print('\n')
+        print("############SIMULATION {}############".format(i))
+        iter_param_dict = grid_search(yaml_filename)
+        # post process: get timers from generated json files
+        out_filename = glob.glob('ctest-*.json')
+        if i != 0:
+            old_dict = iter_time_dict
+            new_dict = json_timers(out_filename,casename)
+            iter_time_dict = dict(Counter(old_dict)+Counter(new_dict))
+        else: 
+            iter_time_dict = json_timers(out_filename,casename)
+        #print("ITER_TIME_DICT: ", iter_time_dict)
+    iter_time_dict = {k: v/(num_simu) for k, v in iter_time_dict.items()}
+    iter_time_dict = {k: round(iter_time_dict[k], 4) for k in iter_time_dict}
+    #print("FINAL: ", iter_time_dict)
     # get parameters with corresponding time in ascending order
     pd_output = dict_to_pandas(iter_param_dict, iter_time_dict)
     print(pd_output)
