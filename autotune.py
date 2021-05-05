bf05cd3e6b728600e110f16900f34afdcaaeb483
Carolyn Kao <chkao831@stanford.edu>
2021-05-05 14:54:29 -0700

    Grid/Random Search formatting done

--- autotune.py
+++ autotune.py
@@ -40,7 +40,7 @@ def write_yaml(dictionary, filename):
         yaml.dump(dictionary, file)
         file.write('...\n')
 
-###################################################################################################
+###############################################################################
 def run_bash(command):
     '''
     Run a bash command
@@ -70,19 +70,104 @@ def run_sim(iter, inFile):
     # Generate output file
     run_bash('cp Testing/Temporary/LastTest.log LastTest_'+str(iter)+'-0.log')
 
-###################################################################################################
+###############################################################################
+def grid_search(inFile, simu):
+    '''
+    Run multiple sims with parameter grid.
+    Parameters:
+        inFile(file): the input yaml file
+        simu(integer): a nonneg integer that represents the current round of simulation
+    Returns:
+        iter_param_dict(dictionary): {#iter:{parameter_to_change:new_value}}
+    '''
+    # Read input file
+    inputDict = read_yaml(inFile)
 
-def get_truncated_normal(mean=1, sd=0.5, low=0.5, upp=1.8):
-    return truncnorm(
-        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+    # Extract MueLu dictionary
+    linsolDict = inputDict['ANONYMOUS']['Piro']['NOX']['Direction']['Newton']['Stratimikos Linear Solver']
+    muDict = linsolDict['Stratimikos']['Preconditioner Types']['MueLu']
 
-def get_truncated_expon(low=0.7, upp=1.7, scale=0.76):
-    return truncexpon(b=(upp-low)/scale, loc=low, scale=scale)
+    # Parameter to change
+    paramList = muDict['Factories']['mySmoother1']['ParameterList']
+    
+    # Define Grid: np.arange(start[inclusive], stop[exclusive], step)
+    param_grid = {'relaxation: damping factor': list(np.arange(0.8, 1.1, 0.1).round(decimals=1)),
+                  'relaxation: sweeps': list(np.arange(1, 2, 1))
+                 # add more params here to tune if applicable
+                 }
+    grid = ParameterGrid(param_grid)
+    
+    # Run simulations
+    ite = 0
+    iter_param_dict = dict()
+    for params in grid:
+        # Change parameter
+        for key, value in params.items():
+            if key in paramList: 
+                paramList[key] = value.item()
+                print('{0}: {1}'.format(key, value.item()))
+        iter_param_dict.update({str(ite):params})
+        # Write input file
+        write_yaml(inputDict, inFile)
+        # Run yaml input file
+        run_sim(ite, inFile)
+        ite = ite + 1
+    print('\n')
+    print('######### TOTAL NUM OF CASES IN EACH SIMULATION: {} #########'.format(ite))
+    print('#################### END OF SIMULATION {} ###################'.format(simu))
+    # Convert logs to json
+    run_bash('python ctest2json.py')
+    return iter_param_dict
 
-def random_search(inFile, n_iter):
+def get_time_gridsearch(filenames, case, simu, iter_time_dict):
+    '''
+    Return a dictionary with time extracted from the output json files
+    Parameters:
+        filenames(list): a list that contains ctest-*.json in this directory
+        case(string): a string that represents the targeted casename from output
+        simu(integer): a nonneg integer that represents the current round of simulation
+        iter_time_dict(dictionary): a passed-in dictionary {#iter:list_of_Albany_Total_Time}
+                                    in which the list of time needs to be updated
+    Returns:
+        iter_time_dict(dictionary): {#iter:list_of_Albany_Total_Time}
+                                    where each #iter represents a unique iter id
+                                    and the list of time is updated
+    '''
+    for each_file in filenames:
+        with open(each_file) as f:
+            dat = json.load(f)
+            # ensure the test passed to get timer -- otherwise set to arbitrary large
+            if dat.get(case, {}).get('passed') is True:
+                time = dat.get(case, {}).get('timers', {}).get('Albany Total Time:')
+            else:
+                time = float('inf')
+            # extract time value and add to dictionary
+            output_num = each_file.split('-')[-1]
+            output_num = os.path.splitext(output_num)[0]
+            if simu == 0: 
+                iter_time_dict[output_num] = list() # for the 1st time, initialize empty list
+            iter_time_dict[output_num].append(time) # update the list of time
+    return iter_time_dict
+
+###############################################################################
+def get_truncated_normal(mean=1, sd=0.3, low=0.5, upp=1.5):
+    '''
+    Generate a truncated normal continuous distribution with range.
+    '''
+    return truncnorm(a=(low-mean)/sd, b=(upp-mean)/sd, loc=mean, scale=sd)
+
+def get_truncated_expon(low=0.7, upp=1.4, sd=0.3):
+    '''
+    Generate a truncated exponential continuous distribution with range.
+    '''
+    return truncexpon(b=(upp-low)/sd, loc=low, scale=sd)
+
+def random_search(inFile, n_iter, seed):
     '''
     Parameters:
         inFile(file): the input yaml file
+        n_iter(integer): a nonneg integer that represents the current round of iteration
+        seed(integer): an integer between 0 and 2**32 - 1 inclusive
     Returns:
         iter_param_dict(dictionary): {#iter:{parameter_to_change:new_value}}
     '''
@@ -96,12 +181,12 @@ def random_search(inFile, n_iter):
     # Parameter to change
     paramList = muDict['Factories']['mySmoother1']['ParameterList']
 
-    # Define Random Search
-    random_state = np.random.RandomState(100)
+    # Define Random State with the Mersenne Twister pseudo-random number generator.
+    random_state = np.random.RandomState(seed)
 
-    #DAMPING_FACTOR = get_truncated_normal()
-    DAMPING_FACTOR = get_truncated_expon()
-    SWEEPS = list(range(1, 5))
+    DAMPING_FACTOR = get_truncated_normal()
+    #DAMPING_FACTOR = get_truncated_expon()
+    SWEEPS = list(range(1, 3))
 
     param_distributions = {'relaxation: damping factor': DAMPING_FACTOR,
                            'relaxation: sweeps': SWEEPS}
@@ -114,31 +199,27 @@ def random_search(inFile, n_iter):
     iter_param_dict = dict()
     for params in sampler:
         print('\n')
-        print("############ CASE {} ############".format(ite))
+        print("########################## CASE {} ##########################".format(ite))
         # Change parameter
         for key, value in params.items():
-            
             if key in paramList: 
-                #print(type(value))
+                # round float to 4 digits
                 if not isinstance(value, int): value = (float)(round(value, 4)) 
-                #print(type(value))
-                print(key)
-                print(value)
+                print('{0}: {1}'.format(key, value))
                 paramList[key] = value
-            
         iter_param_dict.update({str(ite):params})
         # Write input file
         write_yaml(inputDict, inFile)
         run_sim(ite, inFile)
         ite = ite + 1
     print('\n')
-    print('######### TOTAL NUM OF CASES: {} #########'.format(ite))
+    print('################### TOTAL NUM OF CASES: {} ##################'.format(ite))
     run_bash('python ctest2json.py')
     return iter_param_dict
 
 def get_time_randomsearch(filenames, case):
     '''
-    Return a dictionary with time from output json files
+    Return a dictionary with time extracted from the output json files
     Parameters:
         filenames(list): a list that contains ctest-*.json in this directory
         case(string): a string that represents the targeted casename from output
@@ -159,83 +240,8 @@ def get_time_randomsearch(filenames, case):
             output_num = os.path.splitext(output_num)[0]
             iter_time_dict.update({output_num:time})
     return iter_time_dict
-###################################################################################################
 
-def grid_search(inFile, simu):
-    '''
-    Run multiple sims with parameter grid.
-    Parameters:
-        inFile(file): the input yaml file
-    Returns:
-        iter_param_dict(dictionary): {#iter:{parameter_to_change:new_value}}
-    '''
-    # Read input file
-    inputDict = read_yaml(inFile)
-
-    # Extract MueLu dictionary
-    linsolDict = inputDict['ANONYMOUS']['Piro']['NOX']['Direction']['Newton']['Stratimikos Linear Solver']
-    muDict = linsolDict['Stratimikos']['Preconditioner Types']['MueLu']
-
-    # Parameter to change
-    paramList = muDict['Factories']['mySmoother1']['ParameterList']
-    
-    # Define Grid: np.arange(start[inclusive], stop[exclusive], step)
-    param_grid = {'relaxation: damping factor': list(np.arange(0.8, 1.0, 0.1).round(decimals=1)),
-                  'relaxation: sweeps': list(np.arange(1, 2, 1))
-                 # add more params here to tune if applicable
-                 }
-    grid = ParameterGrid(param_grid)
-    
-    # Run simulations
-    ite = 0
-    iter_param_dict = dict()
-    for params in grid:
-        # Change parameter
-        for key, value in params.items():
-            if key in paramList: paramList[key] = value.item()
-        iter_param_dict.update({str(ite):params})
-        # Write input file
-        write_yaml(inputDict, inFile)
-        # Run yaml input file
-        run_sim(ite, inFile)
-        ite = ite + 1
-    print('\n')
-    print('######### TOTAL NUM OF CASES IN EACH SIMULATION: {} #########'.format(ite))
-    print('################### END OF SIMULATION {} ###################'.format(simu))
-    # Convert logs to json
-    run_bash('python ctest2json.py')
-    return iter_param_dict
-
-
-def get_time_gridsearch(filenames, case, ite, iter_time_dict):
-    '''
-    Return a dictionary with time from output json files
-    Parameters:
-        filenames(list): a list that contains ctest-*.json in this directory
-        case(string): a string that represents the targeted casename from output
-    Returns:
-        iter_time_dict(dictionary): {#iter:Albany_Total_Time}
-    '''
-    for each_file in filenames:
-        with open(each_file) as f:
-            dat = json.load(f)
-            # ensure the test passed to get timer -- otherwise set to arbitrary large
-            if dat.get(case, {}).get('passed') is True:
-                time = dat.get(case, {}).get('timers', {}).get('Albany Total Time:')
-            else:
-                time = float('inf')
-            # extract time value and add to dictionary
-            output_num = each_file.split('-')[-1]
-            output_num = os.path.splitext(output_num)[0]
-            if ite == 0:
-                iter_time_dict[output_num] = list()
-            #time_list.append(time)
-            #iter_time_dict.update({output_num:time_list})
-            iter_time_dict[output_num].append(time)
-            #iter_time_dict.update({output_num:time})
-    return iter_time_dict
-
-###################################################################################################
+###############################################################################
 def get_casename(yamlfile):
     '''
     Get the corresponding casename from cmake file to minimize hardcoding
@@ -264,8 +270,8 @@ def dict_to_pandas(param_dict, time_dict):
     Returns:
         df(dataframe): pandas dataframe
     '''
-    print(param_dict)
-    print(time_dict)
+    #print(param_dict)
+    #print(time_dict)
     for key, val in param_dict.items():
         param_dict[key]['time'] = time_dict[key]
     sorted_dict = sorted(param_dict.items(), key = lambda val: (val[1]["time"]))
@@ -278,7 +284,10 @@ def dict_to_pandas(param_dict, time_dict):
 
 def remove_files(yaml_filename):
     '''
-    remove previously-generated files: Tune_*.yaml, *.log, ctest-*.json from this directory
+    Remove previously-generated files: [inputyaml]_[num].yaml, *.log, ctest-*.json 
+    from the current directory.
+    Parameters:
+        yaml_filename(string): the input yaml file name
     '''
     previous_yaml = yaml_filename.split('.')[0] + str("_*.yaml")
     prev_yaml = glob.glob(previous_yaml, recursive=False) #input_albany_Velocity_MueLu_Wedge_Tune_*.yaml 
@@ -290,19 +299,18 @@ def remove_files(yaml_filename):
         except OSError:
             print("Error while deleting file") 
 
-###################################################################################################
+###############################################################################
 if __name__ == "__main__":
     '''
-    Run example
+    Run experiments
     '''
-
     yaml_filename = str()
   
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", type=str,
                     help="YAML input filename (.yaml)")
     parser.add_argument("search", type=str,
-                    help="searching algorithm (grid/random)")
+                    help="Searching algorithm (grid/random)")
     args = parser.parse_args()
     yaml_filename = args.filename
     algo = args.search
@@ -310,26 +318,33 @@ if __name__ == "__main__":
     print("SEARCHING ALGORITHM: ", algo)
     casename = get_casename(yaml_filename)
 
+    # GRID SEARCH
     if algo == "grid":
-        num_simu = int(input("#SIMULATIONS (integer>=1): "))
+        num_simu = int(input("#ROUNDS OF SIMULATIONS (integer>=1): "))
         iter_time_dict = dict()
         for i in range(num_simu):
             remove_files(yaml_filename)
             # perform grid search
             print('\n')
-            print("############ SIMULATION {} ############".format(i))
+            print("####################### SIMULATION {} #######################".format(i))
             iter_param_dict = grid_search(yaml_filename, i)
             # post process: get timers from generated json files
             out_filename = glob.glob('ctest-*.json')
             iter_time_dict = get_time_gridsearch(out_filename, casename, i, iter_time_dict)
-            print("ITER TIME DICT: ", iter_time_dict)
+            #print("ITER TIME DICT: ", iter_time_dict)
+        # take median from all rounds
         iter_time_dict = {k: statistics.median(time_list) for k, time_list in iter_time_dict.items()}
+        # round final digits to 4
         iter_time_dict = {k: round(iter_time_dict[k], 4) for k in iter_time_dict}
-        print("FINAL: ", iter_time_dict)
-    else:
+        #print("FINAL: ", iter_time_dict)
+    # RANDOM SEARCH
+    else: 
         remove_files(yaml_filename)
-        num_randsearch = int(input("#RANDOM SEARCH #ITERS (integer>=1): "))
-        iter_param_dict = random_search(yaml_filename, num_randsearch)
+        num_randsearch = int(input("RANDOM SEARCH #ITERS (integer>=1): "))
+        seed = int(input("RANDOM SEARCH SEED (0<=integer<=2**32): "))
+        # perform random search
+        iter_param_dict = random_search(yaml_filename, num_randsearch, seed)
+        # post process: get timers from generated json files
         out_filename = glob.glob('ctest-*.json')
         iter_time_dict = get_time_randomsearch(out_filename,casename)
 
@@ -337,5 +352,5 @@ if __name__ == "__main__":
     pd_output = dict_to_pandas(iter_param_dict, iter_time_dict)
     print(pd_output)
     csv_out_str = os.path.splitext(yaml_filename)[0] + str('.csv')
-    # print(csv_out_str)
+    #print(csv_out_str)
     pd_output.to_csv(csv_out_str, index=False)
